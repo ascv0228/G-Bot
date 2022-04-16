34903f1ba14e04807684c2d42f7e3db4ed4344a4
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-04-16 11:41:36 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -93,9 +93,9 @@ function cutImageUrl(url) {
 }
 
 function getAvatar(msg) {
-    const member = msg.mentions.users.first() || msg.author;
+    const user = msg.mentions.users.first() || msg.author;
     const avatarEmbed = new Discord.MessageEmbed()
-        .setImage(member.user.displayAvatarURL({ size: 4096, dynamic: true }))
+        .setImage(user.displayAvatarURL({ size: 4096, dynamic: true }))
         .setFooter({
             text: msg.author.tag,
             iconURL: msg.member.displayAvatarURL({ dynamic: true })
