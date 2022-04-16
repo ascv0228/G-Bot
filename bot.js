a177fb1bc7d73e074606870e3dc3067a28392d95
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-04-16 11:21:46 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -93,12 +93,12 @@ function cutImageUrl(url) {
 }
 
 function getAvatar(msg) {
-    const user = message.mentions.users.first() || message.author;
+    const user = msg.mentions.users.first() || msg.author;
     const avatarEmbed = new Discord.RichEmbed()
         .setColor(0x333333)
         .setAuthor(user.username)
         .setImage(user.avatarURL);
-    message.channel.send(avatarEmbed);
+    msg.channel.send(avatarEmbed);
 }
 
 async function confirmReward(msg) {
