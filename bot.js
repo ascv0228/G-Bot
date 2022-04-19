bd236b08a344da65e9e8e6a8c3198a0c976e02a2
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-04-19 15:03:09 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -308,14 +308,17 @@ function checkNotInDatabase(channelId, hashData) {
     else if (channelId == '948120050458574878')
         flag = (collection.count({ '948120050458574878': { $eq: hashData } }) == 0)
     else if (channelId == '863086136180342804') {
-        flag = (new Promise((resolve, reject) => {
+        let temp = new Promise((resolve, reject) => {
             collection.find({ '863086136180342804': { $eq: hashData } }).toArray(function (err, result) {
                 if (err) throw err;
                 if (result == hashData)
                     resolve(result.length);
                 console.log(result.length);
             });
-        }) == 0)
+        })
+        console.log(temp)
+        console.log(typeof temp)
+        flag = (temp == 0)
     }
     console.log(flag)
     return flag
