daed1bbeb0a05e84beaaec80945cbc765c743a43
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-04-20 21:48:47 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -159,7 +159,7 @@ async function AdminFunction(msg) {
 
     }
     else if (msg.content.startsWith(`${prefix}version`)) {
-        msg.reply("ZZ 是巨佬")
+        msg.reply("ZZ 是巨佬, 大GG")
 
     } else if (msg.content.startsWith(`${prefix}dbInit confirm`) && msg.author.id == '411895879935590411') {
         msg.reply('DataBase已清空!')
