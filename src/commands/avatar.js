a397acc594380d9e5b24ef27bdcc197db085f0e2
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-04-22 01:10:53 +0800

    Create avatar.js

---
+++ src/commands/avatar.js
@@ -0,0 +1,20 @@
+const { prefix } = require('../../config/config.json');
+
+module.exports = {
+    name: "avatar",
+    aliases: "avt",
+
+    async execute(client, msg, args) {/*
+        if (!msg.content.startsWith(`${prefix}`)) return;
+        let user = msg.mentions.users.first() || msg.author;
+        const avatarEmbed = new Discord.MessageEmbed()
+            .setImage(user.displayAvatarURL({ size: 4096, dynamic: true }))
+            .setFooter({
+                text: msg.author.tag,
+                iconURL: msg.member.displayAvatarURL({ dynamic: true })
+            });
+        msg.channel.send({ embeds: [avatarEmbed] });*/
+
+        return;
+    }
+};
