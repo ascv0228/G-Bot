65652c0ad47c52a42504212e41991fa319739a4a
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-04-24 22:44:20 +0800

    Update dbGetReward.js

--- src/commands/dbGetReward.js
+++ src/commands/dbGetReward.js
@@ -18,11 +18,10 @@ async function getRewardText(client, msg, args) {
     var d = new Date();
     let output = [`==========${d.getMonth() + 1}/${d.getDate()} 輔助獎勵區==========\n`]
     await temp[0].msg.forEach((value, key) => {
-        let user = await fetchUser(key);
+        let user = fetchUser(key);
         output.push(`x!ticket ${user.tag} ${value}`);
     });
 
-    var d = new Date();
     const attachment = new Discord.MessageAttachment(Buffer.from(output.join('\n')), `${d.getMonth() + 1}/${d.getDate()}.txt`);
     client.channels.cache.get('964516826811858984').send({ files: [attachment] });
 }
