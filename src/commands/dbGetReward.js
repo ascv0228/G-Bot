383028211b96f889a83d1b23025e4073eeef2cd7
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-04-25 00:42:04 +0800

    Update dbGetReward.js

--- src/commands/dbGetReward.js
+++ src/commands/dbGetReward.js
@@ -16,37 +16,33 @@ async function getRewardText(client, msg, args) {
     let temp = await client.Mdbcollection.find({ type: 'reward-ticket' }).toArray();
     //const fetchUser = async id => client.users.fetch(id);
     var d = new Date();
-    let output = await getOutput(client, temp[0].msg);
-
+    let output = [`==========${d.getMonth() + 1}/${d.getDate()} 輔助獎勵區==========\n`];
+    /*
+    Object.keys(user_ids).forEach(async function (key) {
+        console.log(key)
+        let user = await client.users.fetch(key);
+        let userTag = `@${user.username}#${user.discriminator}`
+        console.log(userTag);
+        output.push(`x!ticket ${userTag} ${user_ids[key]}`);
+    });*/
+    for (key in Object.keys(user_ids)) {
+        let user = getUser(client, id);
+        console.log(user);
+        let userTag = `@${user.username}#${user.discriminator}`;
+        console.log(userTag);
+        output.push(`x!ticket ${userTag} ${user_ids[key]}`);
+    }
+    await getOutput(client, temp[0].msg);
     console.log(output);
     const attachment = new Discord.MessageAttachment(Buffer.from(output.join('\n')), `${d.getMonth() + 1}-${d.getDate()}.txt`);
     client.channels.cache.get('964516826811858984').send({ files: [attachment] });
 }
 const waitFor = (ms) => new Promise(r => setTimeout(r, ms))
-async function getOutput(client, user_ids) {
+async function getUser(client, id) {
 
     return new Promise(async (resolve, reject) => {
+        let user = await client.users.cache.get(id);
 
-        var d = new Date();
-        let output = [`==========${d.getMonth() + 1}/${d.getDate()} 輔助獎勵區==========\n`];
-        /*
-        Object.keys(user_ids).forEach(async function (key) {
-            console.log(key)
-            let user = await client.users.fetch(key);
-            let userTag = `@${user.username}#${user.discriminator}`
-            console.log(userTag);
-            output.push(`x!ticket ${userTag} ${user_ids[key]}`);
-        });*/
-        for (key in Object.keys(user_ids)) {
-            let user = await client.users.cache.get(key);
-            console.log(user);
-            console.log(user.tag);
-            let userTag = `@${user.username}#${user.discriminator}`;
-            console.log(userTag);
-            output.push(`x!ticket ${userTag} ${user_ids[key]}`);
-        }
-
-        await waitFor(200)
         resolve(output)
     });
 }
