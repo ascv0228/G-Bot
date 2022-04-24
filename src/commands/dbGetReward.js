8191c8b240d11f6b562463075d86cdc1398672c7
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-04-25 00:23:07 +0800

    Update dbGetReward.js

--- src/commands/dbGetReward.js
+++ src/commands/dbGetReward.js
@@ -18,56 +18,26 @@ async function getRewardText(client, msg, args) {
     var d = new Date();
     let output = await getOutput(client, temp[0].msg);
 
-
-    /*
-    Object.keys(temp[0].msg).forEach(async function (key) {
-        console.log(key)
-        let user = await client.users.fetch(key);
-        let userTag = `@${user.username}#${user.discriminator}`
-        console.log(userTag);
-        output.push(`x!ticket ${userTag} ${temp[0].msg[key]}`);
-    });*/
-
-    /*
-    await temp[0].msg.forEach((value, key) => {
-        let user = fetchUser(key);
-        output.push(`x!ticket ${user.tag} ${value}`);
-    });*/
-
     console.log(output);
     const attachment = new Discord.MessageAttachment(Buffer.from(output.join('\n')), `${d.getMonth() + 1}-${d.getDate()}.txt`);
     client.channels.cache.get('964516826811858984').send({ files: [attachment] });
 }
 
 function getOutput(client, user_ids) {
+    const waitFor = (ms) => new Promise(r => setTimeout(r, ms))
     return new Promise((resolve, reject) => {
 
         var d = new Date();
         let output = [`==========${d.getMonth() + 1}/${d.getDate()} 輔助獎勵區==========\n`];
 
-        Object.keys(user_ids).forEach(async function (key) {
+        await Object.keys(user_ids).forEach(async function (key) {
             console.log(key)
             let user = await client.users.fetch(key);
             let userTag = `@${user.username}#${user.discriminator}`
             console.log(userTag);
             output.push(`x!ticket ${userTag} ${user_ids[key]}`);
         });
-
+        await waitFor(200)
         resolve(output)
-        // //url = cutImageUrl(url);
-        // imageHash(url, 16, true, (error, data) => {
-        //     if (error) {
-        //         resolve('error');
-        //     }
-        //     resolve(data);
-        // })
     });
 }
-
-/*
-async function getCheckMsg(client, msg, args) {
-    let temp = await client.Mdbcollection.find({ type: 'check-msg' }).toArray();
-    jsonString = JSON.stringify({ ...temp }, null, 4);
-    const attachment = new Discord.MessageAttachment(Buffer.from(jsonString, 'utf-8'), 'log.json');
-    msg.author.send({ files: [attachment] });
-}*/
