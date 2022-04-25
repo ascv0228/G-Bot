68c97879a79f8ee268e1ab08b46f19eeaf73e602
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-04-25 11:53:22 +0800

    Up

--- src/commands/dbGetReward.js
+++ src/commands/dbGetReward.js
@@ -1,89 +1,20 @@
-const Discord = require('discord.js');
+
+const RewardUtil = require('../tools/reward-util.js');
 
 module.exports = {
     name: "dbGetReward",
     aliases: ["dbGR"],
 
     execute(client, msg, args) {
-        getRewardText(client, msg, args);  //輔助獎勵
-        getRecordText(client, ["記錄區", "867811395474423838"])
-        getRecordText(client, ["日常獎勵記錄區", "886269472158138429"])
-        getRecordText(client, ["佬專用紀錄區", "948120050458574878"])
+        if (!msg.member.permissions.has('ADMINISTRATOR'))
+            return;
+        RewardUtil.getRewardText(client, msg, args);  //輔助獎勵
+        RewardUtil.getRecordText(client, ["記錄區", "867811395474423838"])
+        RewardUtil.getRecordText(client, ["日常獎勵記錄區", "886269472158138429"])
+        RewardUtil.getRecordText(client, ["佬專用紀錄區", "948120050458574878"])
         return;
     }
 };
-const sendChannel = '964516826811858984'
-
-async function getRewardText(client) {
-    let temp = await client.Mdbcollection.find({ type: 'reward-ticket' }).toArray();
-    //const fetchUser = async id => client.users.fetch(id);
-    var nowDate = new Date().getTime();
-    nowDate += (8 * 60 * 60 * 1000);
-    var date = new Date(nowDate)
-    let output = [`==========${date.getMonth() + 1}/${date.getDate()} 輔助獎勵區==========\n`];
-    const m = new Map(Object.entries(temp[0].msg))
-
-    /*
-    Object.keys(user_ids).forEach(async function (key) {
-        console.log(key)
-        let user = await client.users.fetch(key);
-        let userTag = `@${user.username}#${user.discriminator}`
-        console.log(userTag);
-        output.push(`x!ticket ${userTag} ${user_ids[key]}`);
-    });*/
-    /*
-    for (key in Object.keys(temp[0].msg)) {
-        let user = await getUser(client, key);
-        console.log(user);
-        let userTag = `@${user.username}#${user.discriminator}`;
-        console.log(userTag);
-        output.push(`x!ticket ${userTag} ${temp[0].msg[key]}`);
-    }*/
-
-    m.forEach((value, key) => {
-        //let user = await getUser(client, key);
-        //console.log(user);
-        //let userTag = `@${user.username}#${user.discriminator}`;
-        let userTag = `<@${key}>`;
-        //console.log(userTag);
-        output.push(`x!ticket ${userTag} ${value}`);
-    });
-
-    console.log(output);
-    const attachment = new Discord.MessageAttachment(Buffer.from(output.join('\n')), `${date.getMonth() + 1}-${date.getDate()}.txt`);
-    client.channels.cache.get(sendChannel).send({ files: [attachment] });
-    // client.channels.cache.get(sendChannel).send({ content: '```' + output.join('\n') + '```' });
-}
-
-/*
-client.Mdbcollection.insertOne({ type: 'check-msg', channelId: '963831403001307167', users: new Array() });
-client.Mdbcollection.insertOne({ type: 'check-msg', channelId: '867811395474423838', users: new Array() });
-client.Mdbcollection.insertOne({ type: 'check-msg', channelId: '886269472158138429', users: new Array() });
-client.Mdbcollection.insertOne({ type: 'check-msg', channelId: '948120050458574878', users: new Array() });
-*/
-
-async function getRecordText(client, args) {
-    let temp = await client.Mdbcollection.find({ type: "check-msg", channelId: args[1] }).toArray();
-    let temp2 = temp[0].users.filter(function (elem, pos) {
-        return temp[0].users.indexOf(elem) == pos;
-    })
-    var nowDate = new Date().getTime();
-    nowDate += (8 * 60 * 60 * 1000);
-    var date = new Date(nowDate)
-    let output = [`==========${date.getMonth() + 1}/${date.getDate()} ${args[0]}==========\n`];
-
-
-
-    await temp2.forEach(function getOutput(elem) {
-        let userTag = `<@${elem}>`;
-        output.push(`x!give ${userTag}`);
-    });
-
-    console.log(output);
-    const attachment = new Discord.MessageAttachment(Buffer.from(output.join('\n')), `${date.getMonth() + 1}-${date.getDate()}.txt`);
-    client.channels.cache.get(sendChannel).send({ files: [attachment] });
-    // client.channels.cache.get(sendChannel).send({ content: '```' + output.join('\n') + '```' });
-}
 
 
 
--- src/tools/reward-util.js
+++ src/tools/reward-util.js
@@ -2,6 +2,7 @@ const target_channel = require('../../config/channelId.json');
 const imgUtil = require('./img-util.js');
 const dbUtil = require('./db-util.js');
 const schedule = require('node-schedule');
+const Discord = require('discord.js');
 
 const channelList = [target_channel[0].channel_Id,
 target_channel[1].channel_Id,
@@ -9,16 +10,15 @@ target_channel[2].channel_Id,
 target_channel[3].channel_Id
 ]
 
-module.exports.confirmReward = async function (client, msg) {
-    if (!channelList.includes(msg.channel.id)) return;
-    confirmReward(client, msg);
-};
-
-module.exports.everyScheduleJob = async function (client) {
-    everyScheduleJob(client);
+module.exports = {
+    confirmReward: confirmReward,
+    everyScheduleJob: everyScheduleJob,
+    getRewardText: getRewardText,
+    getRecordText: getRecordText
 };
 
 async function confirmReward(client, msg) {
+    if (!channelList.includes(msg.channel.id)) return;
     let count = await imgUtil.getNotDupeCountFromMsg(client, msg);
     if (count == 0 || count == NaN) return;
 
@@ -42,27 +42,98 @@ async function confirmReward(client, msg) {
 
 }
 
-
 async function giveReward(client) {
     var d = new Date();
     client.channels.cache.get('964516826811858984').send(`==========${d.getMonth() + 1}/${d.getDate()} 輔助獎勵區==========`);
     let temp = await client.Mdbcollection.find({ type: 'reward-ticket' }).toArray();
 
-    new Map(temp[0].msg).forEach((value, key) => {
+    new Map(Object.entries(temp[0].msg)).forEach((value, key) => {
         client.channels.cache.get('964516826811858984').send(`x!bot-ticket <@${key}> ${value}`);
     });
 
 }
-
 
 async function everyScheduleJob(client) {  //https://www.codexpedia.com/javascript/nodejs-cron-schedule-examples/
 
     // var rule1 = new schedule.RecurrenceRule();
     // rule1.minute = new schedule.Range(0, 59, 5);
 
-    schedule.scheduleJob('10 0 16 * * *', async function () {
+    schedule.scheduleJob('50 59 15 * * *', async function () {
         giveReward(client);
+        getRewardText(client);
+        getRecordText(client, ["記錄區", "867811395474423838"])
+        getRecordText(client, ["日常獎勵記錄區", "886269472158138429"])
+        getRecordText(client, ["佬專用紀錄區", "948120050458574878"])
+    });
+
+
+    schedule.scheduleJob('10 0 16 * * *', async function () {
         dbUtil.dbInitReward(client, null);
         dbUtil.dbInitCheckMsg(client, null);
     });
+}
+
+
+const sendChannel = '967986563260772352'
+
+async function getRewardText(client) {
+    let temp = await client.Mdbcollection.find({ type: 'reward-ticket' }).toArray();
+    //const fetchUser = async id => client.users.fetch(id);
+    var nowDate = new Date().getTime();
+    nowDate += (8 * 60 * 60 * 1000);
+    var date = new Date(nowDate)
+    let output = [`==========${date.getMonth() + 1}/${date.getDate()} 輔助獎勵區==========\n`];
+    const m = new Map(Object.entries(temp[0].msg))
+
+    /*
+    Object.keys(user_ids).forEach(async function (key) {
+        console.log(key)
+        let user = await client.users.fetch(key);
+        let userTag = `@${user.username}#${user.discriminator}`
+        console.log(userTag);
+        output.push(`x!ticket ${userTag} ${user_ids[key]}`);
+    });*/
+    /*
+    for (key in Object.keys(temp[0].msg)) {
+        let user = await getUser(client, key);
+        console.log(user);
+        let userTag = `@${user.username}#${user.discriminator}`;
+        console.log(userTag);
+        output.push(`x!ticket ${userTag} ${temp[0].msg[key]}`);
+    }*/
+
+    m.forEach((value, key) => {
+        //let user = await getUser(client, key);
+        //console.log(user);
+        //let userTag = `@${user.username}#${user.discriminator}`;
+        let userTag = `<@${key}>`;
+        //console.log(userTag);
+        output.push(`x!ticket ${userTag} ${value}`);
+    });
+
+    console.log(output);
+    const attachment = new Discord.MessageAttachment(Buffer.from(output.join('\n')), `${date.getMonth() + 1}-${date.getDate()}.txt`);
+    client.channels.cache.get(sendChannel).send({ files: [attachment] });
+    // client.channels.cache.get(sendChannel).send({ content: '```' + output.join('\n') + '```' });
+}
+
+async function getRecordText(client, args) {
+    let temp = await client.Mdbcollection.find({ type: "check-msg", channelId: args[1] }).toArray();
+    let temp2 = temp[0].users.filter(function (elem, pos) {
+        return temp[0].users.indexOf(elem) == pos;
+    })
+    var nowDate = new Date().getTime();
+    nowDate += (8 * 60 * 60 * 1000);
+    var date = new Date(nowDate)
+    let output = [`==========${date.getMonth() + 1}/${date.getDate()} ${args[0]}==========\n`];
+
+    await temp2.forEach(function getOutput(elem) {
+        let userTag = `<@${elem}>`;
+        output.push(`x!give ${userTag}`);
+    });
+
+    console.log(output);
+    const attachment = new Discord.MessageAttachment(Buffer.from(output.join('\n')), `${date.getMonth() + 1}-${date.getDate()}.txt`);
+    client.channels.cache.get(sendChannel).send({ files: [attachment] });
+    // client.channels.cache.get(sendChannel).send({ content: '```' + output.join('\n') + '```' });
 }
