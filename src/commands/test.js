1a5007d08dc4dd74cc33936b9c510680e69ac82c
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-04-26 22:01:19 +0800

    Update test.js

--- src/commands/test.js
+++ src/commands/test.js
@@ -25,11 +25,16 @@ module.exports = {
 
         const guildid = '829673608791851038';
         let guild = await client.guilds.cache.get(guildid);
-        let users = await guild.members.fetch({ user: ['411895879935590411', '765629373084074064'], withPresences: true })
-            .then(console.log)
-        console.log(users['411895879935590411'].user)
-        console.log(users['411895879935590411'].user.username)
-        console.log(users['411895879935590411'].user.discriminator)
+        let members = await guild.members.fetch({ user: ['411895879935590411', '765629373084074064'], withPresences: true })
+        // .then(console.log)
+
+
+        for (const [id, member] of members) {
+            console.log(id, member);
+        }
+        // console.log(users['411895879935590411'].user)
+        // console.log(users['411895879935590411'].user.username)
+        // console.log(users['411895879935590411'].user.discriminator)
         // user.username
         // console.log(user)
         // msg.reply(`${user.tag}`)
