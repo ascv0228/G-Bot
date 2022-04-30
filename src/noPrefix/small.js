951669bfd62b9e3c95a61acd105c305049750cf9
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-05-01 00:48:21 +0800

    Update small.js

--- src/noPrefix/small.js
+++ src/noPrefix/small.js
@@ -2,6 +2,7 @@ module.exports = {
     name: "@小萌新",
     roles: ['948118013293494303'],
     channels: ['832610209377288222', '867811248560144444', '948118924120166410'],
+    permissions: ['ADMINISTRATOR'],
 
     execute(client, msg) {
         let flag = msg.member.roles.cache.has('948118013293494303')
