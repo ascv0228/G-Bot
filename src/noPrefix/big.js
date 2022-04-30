32436469d7f7a23117f09b2c1b7b90060aa98378
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-04-30 23:50:04 +0800

    Update big.js

--- src/noPrefix/big.js
+++ src/noPrefix/big.js
@@ -4,8 +4,8 @@ module.exports = {
     name: "@佬",
     roles: ['938748850112430091'],
 
-    execute(client, msg, args) {
-        let flag = msg.member.roles.has('938748850112430091')
+    execute(client, msg) {
+        let flag = msg.member.roles.cache.has('938748850112430091')
         if (flag)
             msg.reply({ content: '<@&948118013293494303>' });
         return;
