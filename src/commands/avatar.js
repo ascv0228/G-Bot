0603ea521e6c027bb5f963a82d6706846d4d9d42
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-05-10 09:43:51 +0800

    Up

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -6,7 +6,8 @@ module.exports = {
     aliases: ["avt"],
 
     execute(client, msg, args) {
-        let user = (await dcUtil.getUserByTag(msg.guild, args[0])) || msg.author;
+        let user = await dcUtil.getUserByTag(msg.guild, args[0])
+        user = user || msg.author;
         console.log(user)
         const avatarEmbed = new Discord.MessageEmbed()
             .setImage(user.displayAvatarURL({ size: 4096, dynamic: true }))
--- src/commands/memberavatar.js
+++ src/commands/memberavatar.js
@@ -6,7 +6,8 @@ module.exports = {
     aliases: ["memavt"],
 
     execute(client, msg, args) {
-        let user = (await dcUtil.getMemberByTag(msg.guild, args[0])) || msg.member;
+        let user = await dcUtil.getMemberByTag(msg.guild, args[0])
+        user = user || msg.member;
         console.log(user)
         const avatarEmbed = new Discord.MessageEmbed()
             .setImage(user.displayAvatarURL({ size: 4096, dynamic: true }))
