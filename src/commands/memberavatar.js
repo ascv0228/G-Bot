5b71e49490514edf9b8ef29a8e659a6eae7706da
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-05-10 09:40:56 +0800

    Up

--- src/commands/memberavatar.js
+++ src/commands/memberavatar.js
@@ -6,7 +6,7 @@ module.exports = {
     aliases: ["memavt"],
 
     execute(client, msg, args) {
-        let user = dcUtil.getMemberByTag(msg.guild, args[0]) || msg.member;
+        let user = (await dcUtil.getMemberByTag(msg.guild, args[0])) || msg.member;
         console.log(user)
         const avatarEmbed = new Discord.MessageEmbed()
             .setImage(user.displayAvatarURL({ size: 4096, dynamic: true }))
