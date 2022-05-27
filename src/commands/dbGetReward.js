3e81dd2b4b8fcb76d6931b4b8eb4d45b716b02cd
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-05-27 16:08:42 +0800

    Update dbGetReward.js

--- src/commands/dbGetReward.js
+++ src/commands/dbGetReward.js
@@ -8,30 +8,15 @@ module.exports = {
     execute(client, msg, args) {
         if (!msg.member.permissions.has('ADMINISTRATOR'))
             return;
-        RewardUtil.getRewardText(client, msg, args);  //輔助獎勵
-        RewardUtil.getRecordText(client, ["記錄區", "867811395474423838"])
-        RewardUtil.getRecordText(client, ["日常獎勵記錄區", "886269472158138429"])
-        RewardUtil.getRecordText(client, ["佬專用紀錄區", "948120050458574878"])
+        getRewardText(client, guild);
+        getRecordText(client, guild, ["記錄區", "867811395474423838", "normal"])
+        getRecordText(client, guild, ["日常獎勵記錄區", "886269472158138429", "daily"])
+        getRecordText(client, guild, ["佬專用紀錄區", "948120050458574878", "big"])
         return;
     }
 };
 
 
 
-
-
-
-
-
-
-const waitFor = (ms) => new Promise(r => setTimeout(r, ms))
-async function getUser(client, id) {
-
-    return new Promise(async (resolve, reject) => {
-        let user = await client.users.cache.get(id);
-
-        resolve(user)
-    });
-}
 
 
