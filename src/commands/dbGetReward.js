2182a0bcfcb67663c46acd5140ab382c9f894d75
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-05-27 16:10:27 +0800

    Update dbGetReward.js

--- src/commands/dbGetReward.js
+++ src/commands/dbGetReward.js
@@ -8,10 +8,10 @@ module.exports = {
     execute(client, msg, args) {
         if (!msg.member.permissions.has('ADMINISTRATOR'))
             return;
-        getRewardText(client, guild);
-        getRecordText(client, guild, ["記錄區", "867811395474423838", "normal"])
-        getRecordText(client, guild, ["日常獎勵記錄區", "886269472158138429", "daily"])
-        getRecordText(client, guild, ["佬專用紀錄區", "948120050458574878", "big"])
+        RewardUtil.getRewardText(client, guild);
+        RewardUtil.getRecordText(client, guild, ["記錄區", "867811395474423838", "normal"])
+        RewardUtil.getRecordText(client, guild, ["日常獎勵記錄區", "886269472158138429", "daily"])
+        RewardUtil.getRecordText(client, guild, ["佬專用紀錄區", "948120050458574878", "big"])
         return;
     }
 };
