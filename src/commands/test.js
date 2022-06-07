5ea8d055c56db564b070db427ea4933ed1cc1da3
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-07 20:50:23 +0800

    Update test.js

--- src/commands/test.js
+++ src/commands/test.js
@@ -10,22 +10,24 @@ module.exports = {
 
     async execute(client, msg, args) {
         if (msg.author.id !== '411895879935590411') return;
-        if (!msg.member.permissions.has(this.permissions[0])) return msg.channel.send('You do not have that permission! :x:').then(msg.react('❌'));
-        /*let repUser = message.mentions.members.first();
-        if (!repUser) {
-            message.channel.send('Please mention the user you want to setup the vote for!').then((declineMsg) => {
-                message.react('❌');
-                declineMsg.delete({
-                    timeout: 5000,
-                });
-            });
-            return;
-        }*/
+        if (!msg.member.permissions.has(this.permissions[0]))
+            return msg.channel.send('You do not have that permission! :x:').then(msg.react('❌'));
+        if (args.length == 0) {
+            return msg.channel.send({ content: `g!${this.name} <time in minute> <content>` })
+        }
+        let d1 = new Date().getTime();
+        nowDate += (8 * 60 * 60 * 1000);
+        var date = new Date(nowDate)
+        var date2 = new Data(nowDate + (parseInt(args[0]) * 60 * 1000))
+        let output = [`==========${date.getMonth() + 1}/${date.getDate()} 輔助獎勵區==========\n`];
+
         const repVoteEmbed = new Discord.MessageEmbed();
-        repVoteEmbed.setTitle('Vote for Representative Members :crown:');
-        repVoteEmbed.setFooter({
-            text: `Vote by: ${msg.author.tag}, started on : ${msg.createdAt}`
-        });
+        repVoteEmbed.setTitle(`${msg.author.tag} 發起新活動`)
+            .setDescription(args.slice(1).join("\n") + `\n\n限時${args[0]}分鐘\n於${date2.toString().split(' GMT')[0]}結束`)
+            .setFooter({
+                text: msg.author.tag,
+                iconURL: msg.member.displayAvatarURL({ dynamic: true })
+            });
         msg.delete()
         this_msg = await msg.channel.send({ embeds: [repVoteEmbed], content: "00" })
             .then((msg_) => {
@@ -35,6 +37,7 @@ module.exports = {
                 msg.channel.send({ content: `${id}` })
                 setTimeout(() => {
                     msg.channel.messages.fetch(id).then(msg => msg.delete());
+                    msg.channel.send({ embeds: [repVoteEmbed.setDescription(args.slice(1).join("\n") + "\n結束")], content: "活動結束" })
                 }, "5000")
             });
 
@@ -44,3 +47,13 @@ module.exports = {
 };
 
 
+/*let repUser = message.mentions.members.first();
+if (!repUser) {
+    message.channel.send('Please mention the user you want to setup the vote for!').then((declineMsg) => {
+        message.react('❌');
+        declineMsg.delete({
+            timeout: 5000,
+        });
+    });
+    return;
+}*/
