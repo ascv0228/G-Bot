112337ce1bad6a8b345aca557d721afb52e6f4a9
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-09 18:32:20 +0800

    Update say.js

--- src/commands/say.js
+++ src/commands/say.js
@@ -8,7 +8,6 @@ module.exports = {
     name: "say",
 
     async execute(client, msg, args) {
-        if (msg.author.id !== '411895879935590411') return;
         let str = args.join(" ")
         let roleId = dcUtil.pickUserId(str)
         if (roleId == null)
