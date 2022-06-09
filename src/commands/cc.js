48d0c7d4a4525e8cf5e9a4fb31d8014579ffe3eb
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-09 14:40:57 +0800

    Update cc.js

--- src/commands/cc.js
+++ src/commands/cc.js
@@ -10,24 +10,13 @@ module.exports = {
 
     async execute(client, msg, args) {
         if (msg.author.id !== '411895879935590411') return;
-        dbUtil.dbInitActivityCommand(client, args);
-        /*
-        if (!msg.member.permissions.has(this.permissions[0]))
-            return msg.channel.send('You do not have that permission! :x:').then(msg.react('❌'));
+        setCommand_member_role(client);
 
-        categoryId = '834103866772946944'
-
-        p = [
-            {
-                id: msg.guild.id,
-                deny: [Permissions.FLAGS.VIEW_CHANNEL],
-            },
-            {
-                id: msg.author.id,
-                allow: [Permissions.FLAGS.VIEW_CHANNEL],
-            },
-        ]
-        dcUtil.createTextChannel(msg.guild, "test", categoryId, p)*/
     }
 };
+
+async function setCommand_member_role(client) {
+    let temp = await client.Mdbcollection.find({ type: 'ActivityCommand' }).toArray();
+    console.log(temp)
+}
 
