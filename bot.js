e5d5af80278c0fb019dcc88c318bbb06a8a13f7e
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-13 14:54:31 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -141,12 +141,8 @@ client.on('interactionCreate', async interaction => {
         let content = interaction.values[0];
         if (!content.startsWith(`${prefix}`)) return;
         const [cmd, ...args] = content.slice(prefix.length).trimEnd().split(/\s+/);
-        if (args.pop() != interaction.user.id) return interaction.message.channel.send('You cannot use others\' commands.').then((declineMsg) => {
-            interaction.message.react('❌');
-            declineMsg.delete({
-                timeout: 5000,
-            });
-        });;
+        if (args.pop() != interaction.user.id)
+            return interaction.reply({ content: 'You cannot use others\' commands.', ephemeral: true });
         const exec = client.interactions.get(cmd);
         if (!exec) return;
         exec.execute(client, interaction, args);
