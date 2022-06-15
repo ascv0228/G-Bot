f6f4a76b9414e5cdb6447f95f580a00fd2c18668
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-15 15:32:36 +0800

    Update nosex.js

--- src/commands/nosex.js
+++ src/commands/nosex.js
@@ -10,7 +10,7 @@ module.exports = {
             'https://c.tenor.com/NEl9pS8LFdUAAAAC/inawtf-%E5%B7%A5%E4%B8%89%E5%B0%8F.gif',
             'https://cdn.discordapp.com/attachments/964528474809323610/983338949038735411/7EFD54E2-971D-4EDB-A4F6-2D99AEF86EFC.gif',
             'https://media.discordapp.net/attachments/829678324150042684/972048166474092584/received_402828584784372.gif',
-            'https://media.discordapp.net/attachments/855412492217483274/892051302375391293/image2.gif'
+            'https://cdn.discordapp.com/attachments/964528474809323610/986532513419780096/image2.gif'
         ]
         msg.delete()
             .then(msg.channel.send({ content: urls[Math.random() * urls.length | 0] }));
