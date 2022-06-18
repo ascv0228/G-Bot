361ceacd021d257f4f12d285c9c9bd3790e91f84
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-18 18:23:46 +0800

    Update envelope.js

--- src/noPrefix/envelope.js
+++ src/noPrefix/envelope.js
@@ -6,12 +6,12 @@ module.exports = {
 
     execute(client, msg, args) {
         if (!msg.content.startsWith(`x!`)) return;
-        if (msg.content.startsWith(`x!envelope`)) {
+        if (msg.content.startsWith(`x!envelope`) && args.length > 2) {
             client.channels.cache.get('964699991601995787').send(msg.url);
             client.channels.cache.get('964699991601995787').send("無口令");
             return;
         }
-        if (msg.content.startsWith(`x!pasred`)) {
+        if (msg.content.startsWith(`x!pasred`) && args.length > 3) {
             client.channels.cache.get('964699991601995787').send(msg.url);
             client.channels.cache.get('964699991601995787').send("口令:");
             client.channels.cache.get('964699991601995787').send(msg.content.split(' ').splice(3, 3, '').join(' '));
