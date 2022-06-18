c05be1b6584e9c75fabf0d2393a75609a4a1323a
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-18 18:32:18 +0800

    Update cc.js

--- src/commands/cc.js
+++ src/commands/cc.js
@@ -23,6 +23,7 @@ module.exports = {
         const roleid = '986888997538246748';
         // let guild = await client.guilds.cache.get(guildid)
         let role = await dcUtil.getRoleByID(msg.guild, roleid)
+        console.log(role.name)
         let members = await role.members.map(m => m.user.id);
         console.log(members)
 
