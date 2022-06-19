d6c4ff8cee24415dda56adb42333f6cc962dd87c
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-19 23:25:02 +0800

    Update envelope.js

--- src/noPrefix/envelope.js
+++ src/noPrefix/envelope.js
@@ -6,16 +6,19 @@ module.exports = {
 
     execute(client, msg, args) {
         if (!msg.content.startsWith(`x!`)) return;
-        if (msg.content.startsWith(`x!envelope`) && args.length > 2) {
-            client.channels.cache.get('964699991601995787').send(msg.url);
-            client.channels.cache.get('964699991601995787').send("無口令");
-            return;
+        try {
+            if (msg.content.startsWith(`x!envelope`) && args.length > 2) {
+                client.channels.cache.get('964699991601995787').send(msg.url);
+                client.channels.cache.get('964699991601995787').send("無口令");
+                return;
+            }
+            if (msg.content.startsWith(`x!pasred`) && args.length > 3) {
+                client.channels.cache.get('964699991601995787').send(msg.url);
+                client.channels.cache.get('964699991601995787').send("口令:");
+                client.channels.cache.get('964699991601995787').send(msg.content.split(' ').splice(3, 3, '').join(' '));
+                return;
+            }
         }
-        if (msg.content.startsWith(`x!pasred`) && args.length > 3) {
-            client.channels.cache.get('964699991601995787').send(msg.url);
-            client.channels.cache.get('964699991601995787').send("口令:");
-            client.channels.cache.get('964699991601995787').send(msg.content.split(' ').splice(3, 3, '').join(' '));
-            return;
-        }
+        catch { }
     }
 };
