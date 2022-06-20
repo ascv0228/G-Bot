cb3e5c2b46f07882416791b8bdd19f0a7277746e
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-20 12:16:22 +0800

    Update morning.js

--- src/noPrefix/morning.js
+++ src/noPrefix/morning.js
@@ -1,8 +1,11 @@
 module.exports = {
     name: "早安",
     member: ["411895879935590411"],
+    channels: ['863086136180342804'],
     execute(client, msg) {
         if (!this.member.includes(msg.author.id)) return;
+        if (!this.channels.includes(msg.channel.id) &&
+            !(msg.channel.isThread() && this.channels.includes(msg.channel.parentId))) return;
         switch (msg.author.id) {
             case this.member[0]:
                 return msg.reply({ content: '棗安' });
