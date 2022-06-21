087e69143dbea84002a72c63d07fa4df7053fa9b
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-21 22:25:17 +0800

    Update createrole.js

--- src/commands/createrole.js
+++ src/commands/createrole.js
@@ -11,9 +11,15 @@ module.exports = {
         if (!msg.member.permissions.has(this.permissions[0]))
             return;
         if (!this.guilds.includes(msg.guild.id)) return;
+        /*
         let role = await dcUtil.createRole(msg.guild, 'test');
         let member = await dcUtil.getMemberByID(msg.guild, '848194732143542324')
-        member.roles.add(role.id)
+        member.roles.add(role.id)*/
+        let RoleID = '988804577509904414'
+        let role = await dcUtil.getRoleByID(msg.guild, RoleID)
+        role.setColor('RANDOM')
+            .then(updated => msg.reply(`Color: ${updated.color}`))
+            .catch(console.error);
 
     }
 }
