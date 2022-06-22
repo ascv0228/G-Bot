9a0292bba5537c80f98986d3c6931ac42d0d1787
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-23 03:28:23 +0800

    Update setIcon.js

--- src/commands/setIcon.js
+++ src/commands/setIcon.js
@@ -20,13 +20,44 @@ module.exports = {
         if (!this.guilds.includes(msg.guild.id))
             return msg.reply('只能用在外星群');
         if (args.length == 0)
-            return msg.reply('Need Color Hex');
-        console.log(args[0])
+            return msg.reply('Need __image\'s url__ or __emoji__ or __Color("white"、"白色"、"#FFFFFF"、"透明".....)__');
+        let icon = colorMap[args[0]];
+        if (!icon) {
+            let emoji_id = pickEmojiId(args[0]);
+            if (emoji_id) icon = emoji_url(emoji_id);
+        }
+        if (!icon) {
+            icon = args[0]
+        }
 
         let RoleID = roleMap[msg.guild.id]
         let role = await dcUtil.getRoleByID(msg.guild, RoleID)
         // let org_color = role.hexColor;
-        role.setIcon(args[0])
+        role.setIcon(icon)
+            .then(updated => msg.reply(`Set icon: ${icon}`))
+            .catch(msg.reply(`Set icon: Error`));
 
     }
+}
+let colorMap = {
+    'white': 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdj+P///38ACfsD/QVDRcoAAAAASUVORK5CYII=',
+    '白色': 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdj+P///38ACfsD/QVDRcoAAAAASUVORK5CYII=',
+    'FFFFFF': 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdj+P///38ACfsD/QVDRcoAAAAASUVORK5CYII=',
+    '#FFFFFF': 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdj+P///38ACfsD/QVDRcoAAAAASUVORK5CYII=',
+    'ffffff': 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdj+P///38ACfsD/QVDRcoAAAAASUVORK5CYII=',
+    '#ffffff': 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdj+P///38ACfsD/QVDRcoAAAAASUVORK5CYII=',
+    '透明': 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjMLO0/w8AAwUBrjnr9IQAAAAASUVORK5CYII='
+};
+
+function pickEmojiId(str) {
+    if (!str) return null;
+    const mats = str.match(/^<:(\w+):(\d+)>$/);
+    if (mats) {
+        return mats[2];
+    }
+    return null;
+}
+
+function emoji_url(id) {
+    return `https://cdn.discordapp.com/emojis/${id}.webp?size=480&quality=lossless`
 }
