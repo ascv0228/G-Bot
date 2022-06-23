205f3c59f9dbec6c8fe9ae53305ef139bf9482ad
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-23 21:20:59 +0800

    Update guild-invite.js

--- src/commands/guild-invite.js
+++ src/commands/guild-invite.js
@@ -10,12 +10,15 @@ module.exports = {
         if (msg.author.id !== '411895879935590411') return;
         let guild = msg.guild;
         // if (guild.vanityURLCode) return msg.reply(`https://discord.gg/${vanityURLCode}`);
+        if (guild.vanityURLCode) msg.reply(`https://discord.gg/${vanityURLCode}`);
         const invites = await guild.invites.fetch();
-
-        const codeUses = new Map();
-        invites.each(inv => codeUses.set(inv.code, inv.uses));
-        console.log(codeUses)
-        console.log(invites)
+        for (let [code, inv] of invites) {
+            // if (inv.maxAge == 0) return msg.reply(`https://discord.gg/${code}`);
+            if (inv.maxAge == 0) msg.reply(`https://discord.gg/${code}`);
+        }
+        guild.createInvite({ maxAge: 0, maxUses: 10 })
+            .then(invite => console.log(`Created an invite with a code of ${invite.code}`))
+            .catch(console.error);
 
     }
 }
