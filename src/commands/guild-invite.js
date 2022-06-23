87a89c57269257574503e1f2b347b50bc512cb61
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-24 05:55:58 +0800

    Update guild-invite.js

--- src/commands/guild-invite.js
+++ src/commands/guild-invite.js
@@ -19,13 +19,12 @@ module.exports = {
         //     if (inv.maxAge == 0) msg.reply(`https://discord.gg/${code}`);
         // }
         let channel = guild.systemChannel
-        let channelID = guild.systemChannel
+        let channelID = guild.systemChannelId
         console.log(channel)
         console.log(channelID)
 
-        if (!channel || guild.member(client.user).hasPermission('CREATE_INSTANT_INVITE')) return;
-        await channel
-            .createInvite({ maxAge: 0, maxUses: 0 })
+        if (!channel) return;
+        await channel.createInvite({ maxAge: 0, maxUses: 0 })
             .then(async (invite) => {
                 invites.push(`${guild.name} - ${invite.url}`); // push invite link and guild name to array
             })
