c776de92d813888c355489de5dec5416b51f29e2
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-23 21:24:41 +0800

    Update guild-invite.js

--- src/commands/guild-invite.js
+++ src/commands/guild-invite.js
@@ -9,8 +9,13 @@ module.exports = {
     async execute(client, msg, args) {
         if (msg.author.id !== '411895879935590411') return;
         let guild = msg.guild;
+        guild.fetchVanityData()
+            .then(res => {
+                console.log(`Vanity URL: https://discord.gg/${res.code} with ${res.uses} uses`);
+            })
+            .catch(console.error);
         // if (guild.vanityURLCode) return msg.reply(`https://discord.gg/${vanityURLCode}`);
-        if (guild.vanityURLCode) msg.reply(`https://discord.gg/${vanityURLCode}`);
+        // if (guild.vanityURLCode) msg.reply(`https://discord.gg/${vanityURLCode}`);
         const invites = await guild.invites.fetch();
         for (let [code, inv] of invites) {
             // if (inv.maxAge == 0) return msg.reply(`https://discord.gg/${code}`);
