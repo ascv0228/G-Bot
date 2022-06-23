fc9ac48214b2d8d6830fd932409b4714d1649dd3
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-23 22:37:52 +0800

    Update guild-invite.js

--- src/commands/guild-invite.js
+++ src/commands/guild-invite.js
@@ -9,11 +9,10 @@ module.exports = {
     async execute(client, msg, args) {
         if (msg.author.id !== '411895879935590411') return;
         let guild = msg.guild;
-        guild.fetchVanityData()
-            .then(res => {
-                msg.reply(`https://discord.gg/${res.code}`);
-            })
-            .catch(console.error);
+        let vanity = await guild.fetchVanityData();
+        // if (vanity) return msg.reply(`https://discord.gg/${vanity.code}`);
+        if (vanity) msg.reply(`https://discord.gg/${vanity.code}`);
+
         // if (guild.vanityURLCode) return msg.reply(`https://discord.gg/${vanityURLCode}`);
         // if (guild.vanityURLCode) msg.reply(`https://discord.gg/${vanityURLCode}`);
         const invites = await guild.invites.fetch();
@@ -21,7 +20,7 @@ module.exports = {
             // if (inv.maxAge == 0) return msg.reply(`https://discord.gg/${code}`);
             if (inv.maxAge == 0) msg.reply(`https://discord.gg/${code}`);
         }
-        const channel = guild.channels.fetch({ force: true })
+        const channel = guild.channels.cache
             .filter((channel) => channel.type === 'text')
             .first();
 
