b766895a640ecf431d38c06b5e882b320c35fa52
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-24 18:47:22 +0800

    Create xban.js

---
+++ src/noPrefix/xban.js
@@ -0,0 +1,18 @@
+const dcUtil = require('../tools/dc-util.js');
+module.exports = {
+    name: "x!ban",
+    aliases: [],
+    member: ["832777502848974920"],
+    execute(client, msg) {
+        if (!msg.content.startsWith(`x!ban`)) return;
+        if (!this.member.includes(msg.author.id)) return;
+        let args = msg.content.slice('x!ban'.length).trimEnd().split(/\s+/);
+        let member = await dcUtil.getMemberByTag(msg.guild, args[0]);
+
+        switch (msg.author.id) {
+            case this.member[0]:
+                return msg.reply({ content: `<@${member.id}>(${member.nickname}) 退出伺服器&刪庫` });
+        }
+
+    }
+};
