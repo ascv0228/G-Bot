c79d732005163a06f84a8cd872f1a315aee459ec
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-26 21:44:26 +0800

    Create bot.js

---
+++ src/commands/bot.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+var os = require('os');
+
+module.exports = {
+    name: "bot",
+    member: ['411895879935590411'],
+    async execute(client, msg, args) {
+        const totalram = ((os.totalmem() / 10 ** 6 + " ").split('.')[0]);
+        const freeram = ((os.freemem() / 10 ** 6 + " ").split('.')[0]);
+        const usedram = (((os.totalmem() - os.freemem()) / 10 ** 6 + " ").split('.')[0]);
+        const prctfreeram = (((os.freemem() * 100) / os.totalmem + " ").split('.')[0]);
+        const embed = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setTitle("Statistics")
+            .setDescription("Stats of the bot")
+            .addFields(
+                { name: 'Memory (RAM)', value: `Total Memory: ${totalram}MB\nUsed Memory: ${usedram}MB\nFree Memory: ${freeram}MB\nPercentage Of Free Memory: ${prctfreeram}%`, inline: false },
+            )
+            .setTimestamp()
+            .setFooter(`Requested by ${msg.author.username}`, msg.author.displayAvatarURL({ dynamic: true }))
+
+        msg.channel.send(embed)
+    }
+};
