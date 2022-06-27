06778a0b454f80543bd438a1061d710d7148fb54
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-27 11:37:37 +0800

    Update envelope.js

--- src/noPrefix/envelope.js
+++ src/noPrefix/envelope.js
@@ -3,23 +3,26 @@ const { prefix } = require('../../config/config.json');
 module.exports = {
     name: "envelope",
     aliases: ["pasred"],
+    channels: ['990817755328573441', '964699991601995787'],
 
     execute(client, msg) {
         if (!msg.content.startsWith(`x!`)) return;
         let args = msg.content.slice('x!ban '.length).trim().split(/\s+/);
-        try {
-            if (msg.content.startsWith(`x!envelope`) && args.length > 2) {
-                client.channels.cache.get('964699991601995787').send(msg.url);
-                client.channels.cache.get('964699991601995787').send("無口令");
-                return;
+        for (let chl of this.channels) {
+            try {
+                if (msg.content.startsWith(`x!envelope`) && args.length > 2) {
+                    client.channels.cache.get(chl).send(msg.url);
+                    client.channels.cache.get(chl).send("無口令");
+                    return;
+                }
+                if (msg.content.startsWith(`x!pasred`) && args.length > 3) {
+                    client.channels.cache.get(chl).send(msg.url);
+                    client.channels.cache.get(chl).send("口令:");
+                    client.channels.cache.get(chl).send(msg.content.split(' ').splice(3, 3, '').join(' '));
+                    return;
+                }
             }
-            if (msg.content.startsWith(`x!pasred`) && args.length > 3) {
-                client.channels.cache.get('964699991601995787').send(msg.url);
-                client.channels.cache.get('964699991601995787').send("口令:");
-                client.channels.cache.get('964699991601995787').send(msg.content.split(' ').splice(3, 3, '').join(' '));
-                return;
-            }
+            catch { }
         }
-        catch { }
     }
 };
