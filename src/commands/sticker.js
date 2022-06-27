1f90dc09f0c45be6bde70dd9063d711ed86c0030
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-27 12:41:07 +0800

    Create sticker.js

---
+++ src/commands/sticker.js
@@ -0,0 +1,19 @@
+const dcUtil = require('../tools/dc-util.js');
+let d1 = new Date();
+
+module.exports = {
+    name: "sticker",
+
+    execute(client, msg) {
+        let msg1;
+        if (msg.type === 'REPLY') {
+            msg1 = await msg.fetchReference();
+        }
+        else return msg.reply("要回覆一則有'貼圖'(sticker)的訊息");
+        console.log(msg1)
+    }
+}
+
+function getWeb() {
+    return `https://media.discordapp.net/stickers/917071805422915645.png?size=240`
+}
