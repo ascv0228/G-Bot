0062c72bc272b72ec7c67e56689debed83782250
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-27 19:05:27 +0800

    Update sticker.js

--- src/commands/sticker.js
+++ src/commands/sticker.js
@@ -1,5 +1,5 @@
 const dcUtil = require('../tools/dc-util.js');
-let d1 = new Date();
+const Discord = require("discord.js")
 
 module.exports = {
     name: "sticker",
@@ -12,10 +12,30 @@ module.exports = {
         else return msg.reply("要回覆一則有'貼圖'(sticker)的訊息");
         console.log(msg1)
         console.log(msg1.stickers)
+        let member = msg.member;
+        for (let s in msg1.stickers) {
+            const avatarEmbed = new Discord.MessageEmbed()
+                .setImage(getWeb_1(s))
+                .setFooter({
+                    text: member.user.tag,
+                    iconURL: member.displayAvatarURL({ dynamic: true })
+                });
+            msg.channel.send({ embeds: [avatarEmbed] });
+            const avatarEmbed2 = new Discord.MessageEmbed()
+                .setImage(getWeb_2(s))
+                .setFooter({
+                    text: member.user.tag,
+                    iconURL: member.displayAvatarURL({ dynamic: true })
+                });
+            msg.channel.send({ embeds: [avatarEmbed2] });
+        }
         // console.log(await msg1.stickers.fetch())
     }
 }
 
-function getWeb() {
-    return `https://media.discordapp.net/stickers/917071805422915645.png?size=240`
+function getWeb_1(id) {
+    return `https://media.discordapp.net/stickers/${id}.png?size=240`
+}
+function getWeb_2(id) {
+    return `https://media.discordapp.net/stickers/${id}.png?size=240&passthrough=true`
 }
