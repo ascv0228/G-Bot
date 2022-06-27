b71ef7e0676d6fa174012e6cea933155de9e4736
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-27 16:19:32 +0800

    up

--- src/interactions/guild-banner.js
+++ src/interactions/guild-banner.js
@@ -9,10 +9,10 @@ module.exports = {
         let member = interaction.member;
 
         let guild = await client.guilds.cache.get(args[0]);
-
+        let format = guild.banner.startsWith('a_') ? "gif" : "png";
         const bannerEmbed = new Discord.MessageEmbed()
             .setDescription(`${guild.name} 橫幅: `)
-            .setImage(guild.bannerURL({ size: 4096, format: 'gif' }))
+            .setImage(guild.bannerURL({ size: 4096, format: format }))
             .setFooter({
                 text: member.user.tag,
                 iconURL: member.displayAvatarURL({ dynamic: true })
