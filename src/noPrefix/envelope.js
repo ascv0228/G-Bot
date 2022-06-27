4219de198f86c7f23ae16fae435718850d4b9563
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-27 21:33:54 +0800

    Update envelope.js

--- src/noPrefix/envelope.js
+++ src/noPrefix/envelope.js
@@ -7,19 +7,16 @@ module.exports = {
 
     async execute(client, msg) {
         if (!msg.content.startsWith(`x!`)) return;
-        console.log(msg.content)
         let [cmd, ...args] = msg.content.slice().trim().split(/\s+/);
-        console.log(cmd)
-        console.log(args)
         for (let chl of this.channels) {
             let channel = await client.channels.cache.get(chl)
             try {
-                if (msg.content.startsWith(`x!envelope`) && args.length > 1) {
+                if (cmd == 'x!envelope' && args.length > 1) {
                     channel.send(msg.url);
                     channel.send("無口令");
                     continue;
                 }
-                if (msg.content.startsWith(`x!pasred`) && args.length > 2) {
+                if (cmd == 'x!pasred' && args.length > 2) {
                     channel.send(msg.url);
                     channel.send("口令:");
                     channel.send(msg.content.split(' ').splice(3, 3, '').join(' '));
