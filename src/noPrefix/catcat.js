ff05c3e382e1ebde24117f341e2bf2b1fdbbb1a0
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-27 08:31:36 +0800

    Update catcat.js

--- src/noPrefix/catcat.js
+++ src/noPrefix/catcat.js
@@ -1,10 +1,13 @@
 module.exports = {
-    name: "貓",
-    aliases: ["喵"],
+    name: "臭貓貓",
+    aliases: ["臭喵喵"],
     guilds: ['829673608791851038', '988795992667193395'],
 
     execute(client, msg) {
-        return msg.reply({ content: `<@832777502848974920>` });
+        try {
+            return msg.reply({ content: `<@832777502848974920>` });
+        }
+        catch { }
     }
 
 };
