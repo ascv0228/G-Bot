ce6897fdc71cf049287ef0178a65a4560a641583
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-29 21:26:03 +0800

    Update getEmojis.js

--- src/commands/getEmojis.js
+++ src/commands/getEmojis.js
@@ -11,16 +11,17 @@ module.exports = {
         let channel = (args[0] == 'b') ? (await client.channels.fetch(channel_id)) : msg.channel;
 
         for (let guild of client.Guilds) {
-            let msg_array = new Array()
+            // let msg_array = new Array()
             channel.send({ content: `ˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇ${guild[1]}ˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇ` })
             for (let [emoji_id, emoji] of await ((await dcUtil.getGuildByID(client, guild[0])).emojis.fetch(null, { force: true }))) {
-                msg_array.push(`${emoji}  ${emoji_id}`)
-                if (msg_array.length == 20) {
-                    await channel.send({ content: `${msg_array.join('\n')}` })
-                    msg_array = new Array()
-                }
+                // msg_array.push(`${emoji}  ${emoji_id}`)
+                // if (msg_array.length == 20) {
+                //     await channel.send({ content: `${msg_array.join('\n')}` })
+                //     msg_array = new Array()
+                // }
+                channel.send({ content: `${emoji}  ${emoji_id}` })
             }
-            if (msg_array.length) await channel.send({ content: `${msg_array.join('\n')}` })
+            // if (msg_array.length) await channel.send({ content: `${msg_array.join('\n')}` })
             channel.send({ content: `^^^^^^^^^^^^^^^${guild[1]}^^^^^^^^^^^^^^^` })
         }
         // switch (args[0]) {a
