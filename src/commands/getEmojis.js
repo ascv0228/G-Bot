b21f635a2285b2d2857a6a9a0c6c4989d778d050
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-29 18:10:02 +0800

    Update getEmojis.js

--- src/commands/getEmojis.js
+++ src/commands/getEmojis.js
@@ -14,13 +14,13 @@ module.exports = {
             let msg_array = new Array()
             channel.send({ content: `ˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇ${guild[1]}ˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇ` })
             for (let [emoji_id, emoji] of await ((await dcUtil.getGuildByID(client, guild[0])).emojis.fetch())) {
-                msg_array.push(`${emoji}  ${emoji_id}`)
+                msg_array.push(`<a:1:${emoji}  ${emoji_id}`)
                 if (msg_array.length == 20) {
                     await channel.send({ content: `${msg_array.join('\n')}` })
                     msg_array = new Array()
                 }
             }
-            await channel.send({ content: `${msg_array.join('\n')}` })
+            if (msg_array.length) await channel.send({ content: `${msg_array.join('\n')}` })
             channel.send({ content: `^^^^^^^^^^^^^^^${guild[1]}^^^^^^^^^^^^^^^` })
         }
         // switch (args[0]) {a
