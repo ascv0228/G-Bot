ab0cc48769ee2ed63fd5c7c06eda66e3570ae398
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-29 15:48:32 +0800

    Update getEmojis.js

--- src/commands/getEmojis.js
+++ src/commands/getEmojis.js
@@ -6,9 +6,9 @@ module.exports = {
 
     async execute(client, msg, args) {
         if (msg.author.id !== '411895879935590411') return;
-        console.log(client.emojis)
+        // console.log(client.emojis)
         for (let guildID of client.Guilds) {
-            for (let emoji of (await dcUtil.getGuildByID(client, guildID)).emojis) {
+            for (let emoji of await ((await dcUtil.getGuildByID(client, guildID)).emojis.fetch({ force: true }))) {
                 console.log(emoji)
             }
         }
