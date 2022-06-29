183a575055d193c3813dfb6e3d65f787f9d2458a
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-29 23:39:40 +0800

    Update addreaction.js

--- src/commands/addreaction.js
+++ src/commands/addreaction.js
@@ -16,10 +16,10 @@ module.exports = {
 };
 
 async function myAddReaction(client, msg, args) {
-    return (replyMsgAddEmoji(client, msg, args)
-        || useMsgUrl(client, msg, args)
-        || useChannelAndMsgId(client, msg, args)
-        || msg.reply('`1.(using reply)` ' + `${prefix}${this.name}  <reaction>\n`
+    return (await replyMsgAddEmoji(client, msg, args)
+        || await useMsgUrl(client, msg, args)
+        || await useChannelAndMsgId(client, msg, args)
+        || await msg.reply('`1.(using reply)` ' + `${prefix}${this.name}  <reaction>\n`
             + '`2.             ` ' + `${prefix}${this.name} <msg_url> <reaction>`
             + '`3.             ` ' + `${prefix}${this.name} <channel_Id> <msg_Id> <reaction>`))
     // if (msg.type === 'REPLY') {
