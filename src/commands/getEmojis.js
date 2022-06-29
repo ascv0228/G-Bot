35c1e7de4f17cc8fabe686d3460790dd94f26938
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-06-29 17:03:01 +0800

    Update getEmojis.js

--- src/commands/getEmojis.js
+++ src/commands/getEmojis.js
@@ -7,38 +7,26 @@ module.exports = {
     async execute(client, msg, args) {
         if (msg.author.id !== '411895879935590411') return;
         // console.log(client.emojis)
-        // for (let guildID of client.Guilds) {
-        // for (let emoji of await ((await dcUtil.getGuildByID(client, guildID)).emojis.fetch({ force: true }))) {
-        switch (args[0]) {
-            case 'a':
-                msg.guild.emojis.fetch({ force: true })
-                    .then(emojis => {
-                        for (let emoji of emojis) {
-                            console.log(`${emoji}`)
-                        }
-                    })
-                    .catch(console.error);
-                break
-            case 'b':
-                msg.guild.emojis.fetch({ force: true })
-                    .then(emojis => console.log(`${emojis}`))
-                    .catch(console.error);
-                break
-            case 'c':
-                const emojis = msg.guild.emojis.cache
-                    .map((e) => `${e}  -> \`${e.id}\``)
-                    .join(', ');
-                // console.log(emojis)
-                console.log(emojis.size)
-                break
-
-            case 'd':
-                msg.guild.emojis.fetch()
-                    .then(emojis => console.log(`${emojis.size}`))
-                    .catch(console.error);
-                break
-
+        let channel = client.channels.fetch((args[0] == 'a') ? '991628130336907315' : '991629663900270632');
+        if (args[0] == 'a') {
+            client.channels.fetch('991628130336907315')
         }
+
+        for (let guild of client.Guilds) {
+            for (let emoji of await ((await dcUtil.getGuildByID(client, guild[0])).emojis.fetch())) {
+                channel.send({ content: `${emoji.toString()}  ${emoji.id}` })
+            }
+        }
+        // switch (args[0]) {
+
+
+        //     case 'd':
+        //         msg.guild.emojis.fetch()
+        //             .then(emojis => console.log(`${emojis.size}`))
+        //             .catch(console.error);
+        //         break
+
+        // }
         // for (let emoji of await msg.guild.emojis.fetch({ force: true })) {
         //     console.log(emoji)
         // }
