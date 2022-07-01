f89cff56da24f9a681dab59e0e200d641036bc1b
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-07-01 13:24:14 +0800

    Update getBigEmoji.js

--- src/commands/getBigEmoji.js
+++ src/commands/getBigEmoji.js
@@ -1,4 +1,5 @@
 const dcUtil = require('../tools/dc-util.js');
+const imgUtil = require('../tools/img-util.js');
 module.exports = {
     name: "getBigEmoji",
     aliases: ['gbe'],
@@ -8,6 +9,11 @@ module.exports = {
         if (!emoji_id) return;
         msg.reply({ content: `${emoji_url_png(emoji_id)}` });
         msg.reply({ content: `${emoji_url_gif(emoji_id)}` });
+        // console.log(await getBase64FromImageUrl(emoji_url_gif(emoji_id)))
+        // console.log(await getBase64FromImageUrl(emoji_url_png(emoji_id)))
+        let Url = 'https://cdn.discordapp.com/emojis/989663551385927712.png'
+
+        console.log(await getUrlResopne(Url));
     }
 }
 
@@ -18,4 +24,33 @@ function emoji_url_png(id) {
 
 function emoji_url_gif(id) {
     return `https://cdn.discordapp.com/emojis/${id}.gif?size=4096&quality=lossless`
+}
+
+async function getUrlResopne(url) {
+    let request = new XMLHttpRequest();
+    request.open("HEAD", url, true);
+    request.send();
+    return request.onreadystatechange = async function () {
+        if (this.readyState == this.HEADERS_RECEIVED) {
+
+            // Get the raw header string
+            var headers = request.getAllResponseHeaders();
+
+            // Convert the header string into an array
+            // of individual headers
+            var arr = headers.trim().split(/[\r\n]+/);
+
+            var headerMap = {};
+            // Create a map of header names to values
+            for (let line of arr) {
+                var parts = line.split(': ');
+                var header = parts.shift();
+                var value = parts.join(': ');
+                headerMap[header] = value;
+            };
+            console.log(headerMap)
+            return headerMap
+        }
+    }
+
 }
