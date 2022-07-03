f5174b5b4b56780a2727a97f54e1869c8b166b6c
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-07-03 18:06:17 +0800

    Update user-info.js

--- src/commands/user-info.js
+++ src/commands/user-info.js
@@ -7,16 +7,15 @@ module.exports = {
     guilds: [],
 
     async execute(client, msg, args) {
-        if (args.length == 0) return;
         let mention = (await dcUtil.getMemberByTag(msg.guild, args[0])) || msg.member;
         let member = msg.member
-        console.log(mention)
+        console.log(mention.presence)
         const infoEmbed = new Discord.MessageEmbed()
             .setTitle(`${mention.user.tag}使用者資訊`)
             .setThumbnail(mention.displayAvatarURL({ size: 4096, dynamic: true }))
             .addField('暱稱', `${mention.nickname || mention.user.username}`)
             .addField('ID', `${mention.id}`, true)
-            .addField('成員狀態', `${mention.presence ? null : mention.presence.status}`)
+            // .addField('成員狀態', `${mention.presence ? null : mention.presence.status}`)
             .addField('建立時間', `${mention.user.createdAt.toLocaleString('zh-TW', { timeZone: 'UTC' })}`)
             .addField('加入伺服器時間', `${mention.joinedAt.toLocaleString('zh-TW', { timeZone: 'UTC' })}`)
             .addField('<a:nitro:993077592754229288>加成伺服器時間', `${mention.premiumSince.toLocaleString('zh-TW', { timeZone: 'UTC' })}`)
@@ -26,6 +25,10 @@ module.exports = {
                 iconURL: member.displayAvatarURL({ dynamic: true })
             });
         msg.channel.send({ embeds: [infoEmbed] });
+
+        console.log(mention.roles)
+        console.log(mention.presence)
+        console.log(mention.presence.status)
         // 入群時間
         // 創建時間
         // 暱稱
