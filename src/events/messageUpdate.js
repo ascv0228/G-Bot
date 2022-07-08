50a79106efec2261c2734a9ce499dd581175871f
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-07-08 15:04:29 +0800

    Update messageUpdate.js

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -11,6 +11,11 @@ client.on('messageUpdate', async function (oldMessage, newMessage) {
     }
     if (newMessage.channel.id == '964516826811858984') {
         let channel = await client.channels.fetch('964516826811858984')
-        channel.send({ content: '```' + `${newMessage.member.tag} 在記錄區更改文字\n` + `(old) :${oldMessage.content}\n` + `=> (new) :${newMessage.content}` })
+        let messageCreateAt_TW = oldMessage.createAt + 8 * 60 * 60 * 1000
+        let nowDate_TW = new Date() + (8 * 60 * 60 * 1000);
+        let Title = (messageCreateAt_TW.setHours(0, 0, 0, 0) == nowDate_TW.setHours(0, 0, 0, 0)) ?
+            '✅' + '編輯同一天的訊息' : '❌' + '編輯不同一天的訊息'
+        if (oldMessage.createAt)
+            channel.send({ content: '```' + `${newMessage.member.user.tag} 在記錄區更改文字\n` + Title + '\n' + `(old) :${oldMessage.content}\n` + `=> (new) :${newMessage.content}` + '```' })
     }
 })
