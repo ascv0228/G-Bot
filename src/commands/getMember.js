049426423d30f32474daaa21702da6cafa695212
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-07-12 20:18:16 +0800

    Update getMember.js

--- src/commands/getMember.js
+++ src/commands/getMember.js
@@ -10,21 +10,33 @@ module.exports = {
     async execute(client, msg, args) {
         if (!msg.member.permissions.has('ADMINISTRATOR'))
             return;
-        let member = await dcUtil.getMemberByTag(msg.guild, args[0]) || msg.member;
-        console.log(member)
-        /*
+        // let member = await dcUtil.getMemberByTag(msg.guild, args[0]) || msg.member;
+        // console.log(member)
+
         let members = await msg.guild.members.fetch({ force: true })
         console.log(members.size)
-        let order_userTag = new Map();
         let output = new Array();
         for (const [id, member] of members) {
-            let userTag = `x!point <@${id}> 2`;
+            let userTag = `{"member": "${member.user.tag}", "id": "${id}"}, `;
             output.push(userTag)
         }
-    
+
         const attachment = new Discord.MessageAttachment(Buffer.from(output.join('\n')), `log.txt`);
         msg.author.send({ files: [attachment] });
-        */
+        output = new Array();
+        for (const [id, member] of members) {
+            let userTag = `{"member": "${member.user.tag}", "id": "${id}"}, `;
+            output.push(userTag)
+        }
+        let mem1 = await msg.guild.roles.get('962282549021929532').members.map(m => `{"member": "${m.user.tag}", "id": "${m.user.id}"}, `);
+        const attachment1 = new Discord.MessageAttachment(Buffer.from(mem1.join('\n')), `ET1.txt`);
+        msg.author.send({ files: [attachment1] });
+        let mem2 = await msg.guild.roles.get('962283032683880548').members.map(m => `{"member": "${m.user.tag}", "id": "${m.user.id}"}, `);
+        const attachment2 = new Discord.MessageAttachment(Buffer.from(mem2.join('\n')), `ET2.txt`);
+        msg.author.send({ files: [attachment2] });
+        let mem3 = await msg.guild.roles.get('962283113097101322').members.map(m => `{"member": "${m.user.tag}", "id": "${m.user.id}"}, `);
+        const attachment3 = new Discord.MessageAttachment(Buffer.from(mem3.join('\n')), `ET3.txt`);
+        msg.author.send({ files: [attachment3] });
         return;
     }
 };
