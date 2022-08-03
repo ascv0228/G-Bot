779dff1ff8224f761ea12e38cde175362dd012df
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-08-03 18:25:33 +0800

    Update guildMemberAdd.js

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -4,4 +4,7 @@ client.on('guildMemberAdd', member => {
     if (member.guild.id == '829673608791851038') {
         member.roles.add('986888997538246748');
     }
+    if (member.guild.id == '1002583252923596820') {
+        member.roles.add('1004332619971956777')
+    }
 });
