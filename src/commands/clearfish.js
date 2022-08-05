b10804772e90a81bde4fab67943399bc27dddb30
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-08-05 16:43:41 +0800

    Update clearfish.js

--- src/commands/clearfish.js
+++ src/commands/clearfish.js
@@ -6,12 +6,10 @@ module.exports = {
             return;
         let categoryId = '1005021325519233106'
         let category = await client.channels.fetch(categoryId)
-        console.log(categoryId)
-        console.log(category)
-        let c2 = msg.guild.channels.cache.get(categoryId)
-        console.log(c2)
-        let c3 = msg.guild.channels.cache.find(channel => channel.type == "GUILD_CATEGORY")
-        console.log(c2)
+        let chl = category.children
+        let ch2 = ch1.cache
+        console.log(chl)
+        console.log(ch2)
     }
 };
 
