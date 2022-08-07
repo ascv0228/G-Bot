cded50090933259bf91600e622a6dec9190d4116
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-08-07 23:56:08 +0800

    Update clearfish.js

--- src/commands/clearfish.js
+++ src/commands/clearfish.js
@@ -2,19 +2,16 @@ module.exports = {
     name: "clearfish",
     permissions: ['ADMINISTRATOR'],
     async execute(client, msg, args) {
-        let channelName = ''
         if (!msg.member.permissions.has(this.permissions[0]))
             return;
         let categoryId = '1005021325519233106'
         let category = await client.channels.fetch(categoryId)
-        let chl = category.children
+        let chl = category.children.cache
         for (let [id, channel] of chl) {
             let testchl = await channel.clone()
-            testchl.setParent({ channel: categoryId, lockPermissions: false })
+            testchl.setParent(categoryId, { lockPermissions: false })
             channel.delete()
             testchl.send('這就是 #' + testchl.name + ' 頻道的起點')
-            console.log(testchl.name)
         }
     }
 };
-
