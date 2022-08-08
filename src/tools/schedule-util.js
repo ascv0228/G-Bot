7cf7735d6b66c732b26a86b3784176d9d25d79b1
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-08-09 00:01:49 +0800

    Update schedule-util.js

--- src/tools/schedule-util.js
+++ src/tools/schedule-util.js
@@ -103,7 +103,7 @@ async function scheduleJob_CloneTextChannel(client) {
     schedule.scheduleJob('50 59 15 * * *', async function () {
         let categoryId = '1005021325519233106'
         let category = await client.channels.fetch(categoryId)
-        let chl = category.children.cache
+        let chl = category.children
         for (let [id, channel] of chl) {
             let testchl = await channel.clone()
             testchl.setParent(categoryId, { lockPermissions: false })
