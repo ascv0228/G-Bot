db6ce07768815ba548a96db30742f350017b0b3c
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-08-13 13:06:10 +0800

    Update setcolor.js

--- src/commands/setcolor.js
+++ src/commands/setcolor.js
@@ -15,17 +15,18 @@ module.exports = {
     Owner: '411895879935590411',
 
     async execute(client, msg, args) {
-        if (!this.members.includes(msg.author.id))
-            return;
-        if (!this.guilds.includes(msg.guild.id))
-            return msg.reply('只能用在外星群');
-        if (args.length == 0)
-            return msg.reply('Need Color Hex');
+        let role = msg.guild.roles.cache.find(role => role.name === `${member.user.id}`);
+        if (!role)
+            return msg.reply({ content: '無可用私人的身分組' })
+        // if (!this.members.includes(msg.author.id))
+        //     return;
+        // if (!this.guilds.includes(msg.guild.id))
+        //     return msg.reply('只能用在外星群');
+        // if (args.length == 0)
+        //     return msg.reply('Need Color Hex');
         if (getColor(args[0]) == null)
             return msg.reply('Error Color Hex :  #FFFFFF、FFFFFF');
 
-        let RoleID = roleMap[msg.guild.id]
-        let role = await dcUtil.getRoleByID(msg.guild, RoleID)
         let org_color = role.hexColor;
         role.setColor(getColor(args[0]))
             .then(updated => msg.reply(`Color: ${org_color} -> ${updated.hexColor}`))
