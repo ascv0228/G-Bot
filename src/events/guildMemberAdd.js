e992ac581ca88904cb902ae5922e7bd0240a7625
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-08-13 13:10:13 +0800

    Update guildMemberAdd.js

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,6 +1,6 @@
 const client = require("../../bot.js");
 
-client.on('guildMemberAdd', member => {
+client.on('guildMemberAdd', async member => {
     if (member.guild.id == '829673608791851038') {
         member.roles.add('986888997538246748');
     }
