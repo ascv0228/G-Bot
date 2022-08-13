7dc031b02057a99ec465b6f1b659c7ca7cf9a7a6
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-08-13 13:06:13 +0800

    Update guildMemberAdd.js

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -6,5 +6,16 @@ client.on('guildMemberAdd', member => {
     }
     if (member.guild.id == '1002583252923596820') {
         member.roles.add('1004332619971956777')
+        let role = member.guild.roles.cache.find(role => role.name === `${member.user.id}`);
+        if (!role) {
+            let newRole = await member.guild.roles.create({
+                name: `${member.user.id}`,
+                position: 5
+            })
+            member.roles.add(newRole.id)
+        }
+        else {
+            member.roles.add(role.id)
+        }
     }
 });
