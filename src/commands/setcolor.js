3463b04c90a90dc8a93454b0f9772863a4a63391
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-08-14 21:46:12 +0800

    Update setcolor.js

--- src/commands/setcolor.js
+++ src/commands/setcolor.js
@@ -15,38 +15,51 @@ module.exports = {
     Owner: '411895879935590411',
 
     async execute(client, msg, args) {
-        let role = msg.guild.roles.cache.find(role => role.name === `${msg.author.id}`);
 
-        if (!role) {
-            if (msg.guild.id == '1002583252923596820') {
+        if (args.length == 0)
+            return msg.reply('Need Color Hex');
+        if (getColor(args[0]) == null)
+            return msg.reply('Error Color Hex :  #FFFFFF、FFFFFF');
+
+        let role;
+
+        switch (msg.guild.id) {
+            case '1002583252923596820':
+                role = msg.guild.roles.cache.find(role => role.name === `${msg.author.id}`);
+                if (!!role)
+                    break;
                 let pos = msg.guild.roles.cache.get('1004332619971956777').position
                 role = await msg.guild.roles.create({
                     name: `${msg.author.id}`,
                     position: pos
                 })
-            }
+                break;
 
-            else {
+            case '829673608791851038':
+                if (!msg.member.roles.cache.has('988641623384662066'))
+                    return msg.reply({ content: '無可用私人的身分組' })
+                role = await msg.guild.roles.fetch('988641623384662066');
+                break;
+
+            case '988795992667193395':
+                role = await msg.guild.roles.fetch('988804577509904414');
+                break;
+
+            default:
                 return msg.reply({ content: '無可用私人的身分組' })
-            }
+
+
         }
+
+
         if (!msg.member.roles.cache.has(role.id)) {
             msg.member.roles.add(role.id)
         }
-        if (getColor(args[0]) == null)
-            return msg.reply('Error Color Hex :  #FFFFFF、FFFFFF');
-        // if (!this.members.includes(msg.author.id))
-        //     return;
-        // if (!this.guilds.includes(msg.guild.id))
-        //     return msg.reply('只能用在外星群');
-        // if (args.length == 0)
-        //     return msg.reply('Need Color Hex');
 
         let org_color = role.hexColor;
         role.setColor(getColor(args[0]))
             .then(updated => msg.reply(`Color: ${org_color} -> ${updated.hexColor}`))
             .catch(console.error);
-
     }
 }
 
