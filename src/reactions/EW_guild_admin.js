28fb47ff57dc4bc4f0bd60c14efbe416ed89a4dd
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-08-22 22:10:40 +0800

    Update EW_guild_admin.js

--- src/reactions/EW_guild_admin.js
+++ src/reactions/EW_guild_admin.js
@@ -1,20 +1,45 @@
 const dcUtil = require('../tools/dc-util.js');
+
+let handle_Obj = new Map(Object.entries({
+    '988964977224318986': {
+        something: {
+            'guild': '856793573194465300',
+            'role': '987326459402145852',
+            'permissions': {
+                'messageReactionAdd': [Discord.PermissionFlagsBits.Administrator],
+                'messageReactionRemove': []
+            }
+        },
+        emoji: ['✅'],
+        unable: {
+        },
+        enable: {
+        },
+        clear_other_emoji: true, // 清除 不是選項的emoji
+        clear_options_emoji: false, // 清除 其他選項的emoji
+        clear_this_emoji: false, // 清除自身emoji
+    },
+
+}));
+
 module.exports = {
     name: "EW_guild_admin",
-    message_Id: ['988964977224318986'],
+    message_Id: [...handle_Obj.keys()],
+    handle_Obj: handle_Obj,
 
     async execute(client, event, reaction, user) {
-        if (reaction.emoji.name != '✅') return;
-        let admin_role_id = '987326459402145852';
-        let EW_guild = await client.guilds.cache.get('856793573194465300');
-        const member = await dcUtil.getMemberByID(EW_guild, user.id);
+        let obj = this.handle_Obj.get(reaction.message.id)
+        let role_id = obj.something['role'];
+        let guild = client.guilds.cache.get(obj.something['guild']);
+        if (!role_id) return;
+        let role = await dcUtil.getRoleByID(guild, role_id)
 
         switch (event) {
             case 'messageReactionAdd':
-                member.roles.add(admin_role_id)
+                role.edit({ permissions: obj.something['permissions'][event] })
                 break;
             case 'messageReactionRemove':
-                member.roles.remove(admin_role_id)
+                role.edit({ permissions: obj.something['permissions'][event] })
                 break;
 
         }
