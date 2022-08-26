9b3b9f8f5c3d40a7944710eab6cfff8f01faadf8
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-08-26 13:51:33 +0800

    Update guildMemberAdd.js

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,22 +1,11 @@
 const client = require("../../bot.js");
 
 client.on('guildMemberAdd', async member => {
+    if (!member || !member.user || member.user.bot) return;
     if (member.guild.id == '829673608791851038') {
         member.roles.add('986888997538246748');
     }
     if (member.guild.id == '1002583252923596820') {
         member.roles.add('1004332619971956777')
-        let role = member.guild.roles.cache.find(role => role.name === `${member.user.id}`);
-        if (!role) {
-            let pos = member.guild.roles.cache.get('1004332619971956777').position
-            let newRole = await member.guild.roles.create({
-                name: `${member.user.id}`,
-                position: pos
-            })
-            member.roles.add(newRole.id)
-        }
-        else {
-            member.roles.add(role.id)
-        }
     }
 });
