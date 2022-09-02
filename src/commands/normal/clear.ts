3a17a9257837fe0b77eecf0ed2fe366919d4b448
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-09-02 10:46:50 +0800

    debug

--- src/commands/normal/clear.ts
+++ src/commands/normal/clear.ts
@@ -1,55 +1,55 @@
-import Discord from "discord.js";
-import tools from "../../utils/tools";
-import { ZClient } from "../../structure/client";
-import { CmdType } from "../../utils/types";
+// import Discord from "discord.js";
+// import tools from "../../utils/tools";
+// import { ZClient } from "../../structure/client";
+// import { CmdType } from "../../utils/types";
 
-export = {
-    name: 'clear',
-    aliases: ["clr"],
-    description: '清除頻道聊天紀錄',
-    permissions: ["ManageMessages"],
-    roles: [],
-    type: [CmdType.Universal],
+// export = {
+//     name: 'clear',
+//     aliases: ["clr"],
+//     description: '清除頻道聊天紀錄',
+//     permissions: ["ManageMessages"],
+//     roles: [],
+//     type: [CmdType.Universal],
 
-    async execute(client: ZClient, msg: Discord.Message, args: string[]) {
-        let amount = 100;
+//     async execute(client: ZClient, msg: Discord.Message, args: string[]) {
+//         let amount = 100;
 
-        if (args.length > 0) {
-            if (args[0].match(/^\d+$/)) {
-                amount = parseInt(args[0]);
-            } else {
-                msg.channel.send({ content: '參數需是數字!' });
-                return;
-            }
-        }
+//         if (args.length > 0) {
+//             if (args[0].match(/^\d+$/)) {
+//                 amount = parseInt(args[0]);
+//             } else {
+//                 msg.channel.send({ content: '參數需是數字!' });
+//                 return;
+//             }
+//         }
 
-        const MAX_DELETE_AMOUNT = 100;
-        const INTERVAL = 2000;
+//         const MAX_DELETE_AMOUNT = 100;
+//         const INTERVAL = 2000;
 
-        const count = Math.floor(amount / MAX_DELETE_AMOUNT);
-        const remain = amount % MAX_DELETE_AMOUNT;
+//         const count = Math.floor(amount / MAX_DELETE_AMOUNT);
+//         const remain = amount % MAX_DELETE_AMOUNT;
 
-        await msg.delete();
-        await tools.sleep(INTERVAL);
+//         await msg.delete();
+//         await tools.sleep(INTERVAL);
 
-        const messageDeletor = async (num: number): Promise<void> => {
-            const channel = msg.channel as Discord.TextChannel;
-            const messages = await msg.channel.messages.fetch({ limit: num });
-            const deletedMsgs = await channel.bulkDelete(messages, true);
-            const surplusMsgs = deletedMsgs.size ? messages.filter((_, key) => !deletedMsgs.has(key)) : messages;
+//         const messageDeletor = async (num: number): Promise<void> => {
+//             const channel = msg.channel as Discord.TextChannel;
+//             const messages = await msg.channel.messages.fetch({ limit: num });
+//             const deletedMsgs = await channel.bulkDelete(messages, true);
+//             const surplusMsgs = deletedMsgs.size ? messages.filter((_, key) => !deletedMsgs.has(key)) : messages;
 
-            for (const [_, m] of surplusMsgs) {
-                await m.delete();
-                await tools.sleep(INTERVAL);
-            }
-        };
+//             for (const [_, m] of surplusMsgs) {
+//                 await m.delete();
+//                 await tools.sleep(INTERVAL);
+//             }
+//         };
 
-        for (let i = 0; i < count; ++i) {
-            await messageDeletor(MAX_DELETE_AMOUNT);
-        }
+//         for (let i = 0; i < count; ++i) {
+//             await messageDeletor(MAX_DELETE_AMOUNT);
+//         }
 
-        if (remain > 0) {
-            await messageDeletor(remain);
-        }
-    },
-};
+//         if (remain > 0) {
+//             await messageDeletor(remain);
+//         }
+//     },
+// };
