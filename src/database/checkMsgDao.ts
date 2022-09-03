b2b50d0f58c6f4d4df266b1e2f4658ec8310523f
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-09-03 16:37:51 +0800

    Update checkMsgDao.ts

--- src/database/checkMsgDao.ts
+++ src/database/checkMsgDao.ts
@@ -41,6 +41,7 @@ export default {
         try {
             const collection = db.svr.db('G-Bot').collection('Clients');
             const datas = await collection.find({ type: "check-msg", channelId: channelId }).toArray();
+            console.log(datas)
             user_ids = datas[0].users.filter(function (elem, pos) {
                 return datas[0].users.indexOf(elem) == pos;
             })
@@ -53,6 +54,7 @@ export default {
     async checkInMsg(channelId: string, user_id: string): Promise<boolean> {
         const collection = db.svr.db('G-Bot').collection('Clients')
         let temp = await collection.find({ type: "check-msg", channelId: channelId }).toArray();
+        console.log(temp)
         let user_ids = temp[0].users
         return user_ids.includes(user_id)
     },
