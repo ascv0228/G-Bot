fa3af0ff90834451e9c33a7e8ec6116559de9817
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-09-06 12:40:56 +0800

    Create delete.ts

---
+++ src/commands/owner/delete.ts
@@ -0,0 +1,24 @@
+import Discord from "discord.js";
+import tools from "../../utils/tools";
+import { ZClient } from "../../structure/client";
+import { CmdType } from "../../utils/types";
+import dcUtil from "../../utils/discord-util";
+
+export = {
+    name: "delete",
+    aliases: ['del'],
+
+    description: '刪除訊息',
+    permissions: [],
+    roles: [],
+    type: [CmdType.Owner],
+
+    async execute(client: ZClient, msg: Discord.Message, args: string[]) {
+        if (msg.type !== Discord.MessageType.Reply) {
+            setTimeout(() => msg.reply('需要回覆一個訊息'), 10000);
+            return;
+        }
+        let msg1 = await msg.fetchReference();
+        msg1.delete().catch(() => { });
+    }
+}
