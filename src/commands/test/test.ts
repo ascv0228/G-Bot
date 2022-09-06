ad9f03996448f38bd03a5e00c89be3d67d6eb3fe
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-09-06 13:55:25 +0800

    Update test.ts

--- src/commands/test/test.ts
+++ src/commands/test/test.ts
@@ -15,19 +15,13 @@ export = {
 
 
     async execute(client: ZClient, msg: Discord.Message, args: string[]) {
-        // let msg1 = await msg.fetchReference();
-        // console.log(msg1.reactions)
-        // console.log('=============================')
-        // console.log(msg1.reactions.cache)
-        // console.log('=============================')
-        // const exec = client.embedCommands.find(v => !!(msg1.embeds[0].title.match(v.mat)));
-        // exec.execute(client, msg1, msg1.embeds[0])
-        let roleId = '1010130193840615476'
-        let guild = dcUtil.getGuildByID(client, '1002583252923596820')
-        let role = await dcUtil.getRoleByID(guild, roleId)
-        role.edit({ permissions: Discord.PermissionFlagsBits.Administrator })
-        role.edit({ permissions: [] })
-        // Discord.PermissionFlagsBits.Administrator
+        let msg1 = await msg.fetchReference();
+
+        console.log("msg1.interaction && msg1.interaction.commandName == 'daily': ", msg1.interaction && msg1.interaction.commandName == 'daily')
+
+        console.log("msg1.interaction.user.id != process.env.BOT_OWNER: ", msg1.interaction.user.id != process.env.BOT_OWNER)
+
+        // msg.channel.send(`<@${msg.interaction.user.id}> 今天完成vf-daily`);
 
     }
 };
