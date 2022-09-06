c7477ef7efd8811411bde3244159c98e304cc358
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-09-06 12:40:51 +0800

    update music

---
+++ src/commands/music/playnext.ts
@@ -0,0 +1,72 @@
+import Discord from "discord.js";
+import tools from "../../utils/tools";
+import musicDao from "../../database/musicDao";
+import { ZClient } from "../../structure/client";
+import { CmdType } from "../../utils/types";
+
+export = {
+    name: "playnext",
+    aliases: ["pt"],
+    description: '插入音樂',
+    permissions: [],
+    roles: [],
+    users: [],
+    type: [CmdType.Music],
+
+    async execute(client: ZClient, msg: Discord.Message, args: string[]) {
+        if (!msg.member.voice.channel) {
+            await msg.channel.send({ content: `此命令需在語音頻道中使用!` });
+            return;
+        }
+        if (args.length < 1) {
+            await msg.channel.send({ content: `需提供網址或歌名!` });
+            return;
+        }
+        const player = client.manager.players.get(msg.guild.id);
+        if (!player || player.queue.size <= 0) {
+            await msg.channel.send({ content: `隊列當前無任何曲目 請用 \`${client.prefix}play\`` });
+            return
+        }
+        if (msg.member.voice.channel.id != player.voiceChannel) {
+            await msg.channel.send({ content: `此命令需和機器人相同頻道才可使用!` });
+            return;
+        }
+
+        const res = await client.manager.search({
+            query: args.join(" "),
+            source: "youtube" // youtube or soundcloud
+        },
+            msg.author
+        );
+
+        if (res.loadType == "LOAD_FAILED") throw res.exception;
+
+        if (res.tracks.length <= 0) {
+            await msg.channel.send({ content: `未發現任何曲目!` });
+            return;
+        }
+
+        musicDao.clearMusicByRange(player.guild, 1, player.queue.totalSize)
+        let tempQueue = player.queue.remove(2, player.queue.totalSize);
+
+        switch (res.loadType) {
+            case "PLAYLIST_LOADED":
+                let time = 0;
+                res.tracks.forEach((track) => {
+                    time += track.duration;
+                    player.queue.add(track);
+                });
+
+                await msg.channel.send({ content: `成功加入\`${res.tracks.length}\`首歌曲至隊列 \`(${tools.timeFormat(time)})\`` });
+                break;
+            default:
+                player.queue.add(res.tracks[0]);
+
+                await msg.channel.send({ content: `🎶 已將曲目 \`${res.tracks[0].title}\` 加入隊列` });
+                break;
+        }
+        player.queue.add(tempQueue);
+        musicDao.addMusicsToQueue(player.guild, player.queue);
+
+    },
+};
--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -11,15 +11,36 @@ export = {
     async execute(client: ZClient, oldState: Discord.VoiceState, newState: Discord.VoiceState) {
         if (!auth.isAuthGuild(oldState.guild.id)) return;
         if (!auth.isAuthGuild(newState.guild.id)) return;
+        if (!(oldState.channelId && oldState.channel.members.get(client.user.id))
+            && !(newState.channelId && newState.channel.members.get(client.user.id)))
+            return;
+
+        if (oldState.channelId === newState.channelId) {
+            // return console.log('Mute/Deafen Update');
+        }
+        if (!oldState.channelId && newState.channelId) {
+            // return console.log('Connection Update');
+        }
+        if (oldState.channelId && !newState.channelId) {
+            // return console.log('Disconnection Update');
+            if (oldState.channel.members.size == 1 && oldState.channel.members.get(client.user.id)) {
+                let player = client.manager.players.get(newState.guild.id);
+                if (player) {
+                    player.destroy();
+                }
+            }
+        }
+
+
         if (newState.member.id != client.user.id) return;
 
-        console.log("VoiceStateUpdate ->", oldState.deaf, newState.deaf, oldState.mute, newState.mute);
-        console.log(oldState.channelId, newState.channelId);
-        
+        console.log("VoiceStateUpdate: " + `{deaf: ${oldState.deaf}, mute: ${oldState.mute}} => {deaf: ${newState.deaf}, mute: ${newState.mute}}`);
+        console.log(`channel: ${oldState.channelId} => ${newState.channelId}`);
+
         let player = client.manager.players.get(newState.guild.id);
 
-        if(!newState.channelId) {
-            if(player) {
+        if (!newState.channelId) {
+            if (player) {
                 player.destroy();
             }
         } else {
