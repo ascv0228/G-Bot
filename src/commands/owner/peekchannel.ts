5643898fa3341d0a538d74a262bf23086031c7a3
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-09-19 16:23:08 +0800

    Update peekchannel.ts

--- src/commands/owner/peekchannel.ts
+++ src/commands/owner/peekchannel.ts
@@ -17,13 +17,14 @@ export = {
     async execute(client: ZClient, msg: Discord.Message, args: string[]) {
 
 
-        let channel: Discord.TextChannel | Discord.NewsChannel
+        let channel: Discord.TextChannel | Discord.NewsChannel | Discord.APIGuildForumChannel
         if (args.length && msg.member.id == process.env.BOT_OWNER && args[0] == 'parent' && [10, 11, 12].includes(msg.channel.type)) {
             if ((msg.channel as Discord.TextChannel).parentId) {
+
                 channel = (msg.channel as Discord.ThreadChannel).parent
             }
         }
-        if (!!channel) {
+        if (!channel) {
             channel = (args.length && msg.member.id == process.env.BOT_OWNER) ? await client.channels.fetch(args[0]) as Discord.TextChannel : msg.channel as Discord.TextChannel
         }
 
