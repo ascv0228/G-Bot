7014c2ff1dcc6eda1adae625ebbe535752d8c856
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-10-27 14:38:10 +0800

    suppressEmbeds removeAttachments

---
+++ src/commands/normal/removeAttachments.ts
@@ -0,0 +1,30 @@
+import Discord from "discord.js";
+import tools from "../../utils/tools";
+import { ZClient } from "../../structure/client";
+import { CmdType } from "../../utils/types";
+import auth from "../../utils/auth";
+import dcUtil from "../../utils/discord-util";
+import { Executor } from "../../structure/executor";
+
+export = {
+    name: "removeAttachments",
+    aliases: ["ra", "removeattachments"],
+    description: '對特定訊息刪除附加檔案',
+    permissions: ['Administrator', 'ManageMessages'],
+    roles: [],
+    type: [CmdType.Universal],
+    usage: [
+        ["(using reply)", ""],
+    ],
+    async execute(client: ZClient, msg: Discord.Message, args: string[]) {
+        // if (!this.member.includes(msg.author.id)) return;
+        if (!msg || !args || args.length < 1 || msg.type != Discord.MessageType.Reply) return null;
+        let msg1 = await msg.fetchReference();
+        return msg1.removeAttachments()
+    }
+};
+
+
+
+
+
