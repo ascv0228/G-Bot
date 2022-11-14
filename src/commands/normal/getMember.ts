94727f13f1ef11e140f04bf26973e2c8afc88d43
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2022-11-14 22:02:42 +0800

    Update getMember.ts

--- src/commands/normal/getMember.ts
+++ src/commands/normal/getMember.ts
@@ -19,19 +19,23 @@ export = {
     async execute(client: ZClient, msg: Discord.Message, args: string[]) {
         if (!args.length)
             return msg.reply('需要標記身分組')
-        console.log(args)
-        if (!dcUtil.pickRoleId(args[0]))
-            return msg.reply('未知身分組')
-        let role = await dcUtil.getRoleByID(msg.guild, dcUtil.pickRoleId(args[0]));
-        let members = role.members.keys()
+        for (let arg of args) {
+            if (!dcUtil.pickRoleId(arg)) {
+                msg.reply(`未知身分組 ${arg}`);
+                continue;
+            }
+            let roleId = dcUtil.pickRoleId(arg)
+            let role = await dcUtil.getRoleByID(msg.guild, roleId);
+            let members = role.members.keys()
 
-        let output = new Array();
-        for (const id of members) {
-            output.push(`${id}`)
+            let output = new Array();
+            for (const id of members) {
+                output.push(`${id}`)
+            }
+
+            const attachment = new Discord.AttachmentBuilder(Buffer.from(output.join('\n')), { name: `${role.name}.txt` });
+            msg.author.send({ files: [attachment] });
         }
-
-        const attachment = new Discord.AttachmentBuilder(Buffer.from(output.join('\n')), { name: `${role.name}.txt` });
-        msg.author.send({ files: [attachment] });
 
     }
 };
