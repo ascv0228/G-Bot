a26d1645022ff8102a7c9798d7d2a00ed0341965
qihuan <77038091+ascv0228@users.noreply.github.com>
2022-12-07 12:27:19 +0800

    Update attacker.ts

--- src/eggs/attacker.ts
+++ src/eggs/attacker.ts
@@ -3,17 +3,36 @@ import { ZClient } from "../structure/client";
 import { CmdType } from "../utils/types";
 import dcUtil from "../utils/discord-util";
 import { EggRuleType, EggType } from "../utils/types";
+import dataJson from "../data";
 let rolesMap = {
-    '@輸出': '<@&987180143275569212>',
-    '@输出': '<@&987180143275569212>',
+    '@輸出': {
+        [dataJson['guild']['RD_main']] : '<@&987180143275569212>',
+        [dataJson['guild']['ฅ(•ㅅ•^✿)ฅ']] : '<@&1043508245307867196>'
+    },
+    '@输出': {
+        [dataJson['guild']['RD_main']] : '<@&987180143275569212>',
+        [dataJson['guild']['ฅ(•ㅅ•^✿)ฅ']] : '<@&1043508245307867196>'
+    },
     '<@&987180143275569212>': '<@&987180143275569212>',
-    '@輔助': '<@&987180195121332224>',
-    '@辅助': '<@&987180195121332224>',
+    '<@&1043508245307867196>': '<@&1043508245307867196>',
+    '@輔助': {
+        [dataJson['guild']['RD_main']] : '<@&987180195121332224>',
+        [dataJson['guild']['ฅ(•ㅅ•^✿)ฅ']] : '<@&1043508182665936966>'
+    },
+    '@辅助': {
+        [dataJson['guild']['RD_main']] : '<@&987180195121332224>',
+        [dataJson['guild']['ฅ(•ㅅ•^✿)ฅ']] : '<@&1043508182665936966>'
+    },
     '<@&987180195121332224>': '<@&987180195121332224>',
+    '<@&1043508182665936966>' : '<@&1043508182665936966>',
+    '@自拉' :{
+        [dataJson['guild']['ฅ(•ㅅ•^✿)ฅ']] : '<@&1043854592401752115>'
+    },
+    '<@&1043854592401752115>':'<@&1043854592401752115>'
 }
 export = {
     aliases: Object.keys(rolesMap),
-    channels: ['987233571276210217', '987234093748068363'],
+    channels: ['987233571276210217', '987234093748068363', '1049904141905965076', '1049903667639242842'],
 
     description: '協同區找人',
     roles: [],
@@ -22,8 +41,9 @@ export = {
 
     async execute(client: ZClient, msg: Discord.Message, listen: string) {
         if (dcUtil.pickRoleId(listen) && msg.mentions.roles.get(dcUtil.pickRoleId(listen))) return;
+        let res = (typeof rolesMap[listen] === 'string' || rolesMap[listen] instanceof String)? rolesMap[listen] : rolesMap[listen][msg.guild.id]
 
-        return msg.reply({ content: rolesMap[listen] + `, ${msg.member} 找你` });
+        return msg.reply({ content: res + `, ${msg.member} 找你` });
 
     }
 };
